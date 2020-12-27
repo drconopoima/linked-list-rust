904c7c4410e799a270eb9ee168b2a41160fdf869
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-12-27 23:42:02 +0100

    Generic ListNode LinkedList with Box

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+struct ListNode<T> {
+    value: T,
+    next: Option<Box<ListNode<T>>>
+}
+
+#[derive(Debug)]
+pub struct LinkedList<T> {
+    pub length: u64,
+    head: Option<ListNode<T>>,
+    tail: Option<ListNode<T>>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
