fdfac51e5d4b374dd592a4248d430776e7e2f290
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-12-28 00:26:48 +0100

    linked list struct head & length

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
 #[derive(Debug)]
-struct ListNode<T> {
+struct LinkedNode<T> {
     value: T,
-    next: Option<Box<ListNode<T>>>
+    next: Option<Box<LinkedNode<T>>>
 }
 
 #[derive(Debug)]
 pub struct LinkedList<T> {
     pub length: u64,
-    head: Option<ListNode<T>>,
-    tail: Option<ListNode<T>>,
+    head: Option<Box<LinkedNode<T>>>,
+    // tail: Option<Box<LinkedNode<T>>>,
+}
+
+impl<T> LinkedNode<T> {
+    fn new(value: T, next: Option<Box<LinkedNode<T>>>) -> LinkedNode<T> {
+        LinkedNode {
+            value,
+            next
+        }
+    }
+}
+
+impl<T> LinkedList<T> {
+    fn new(node: Option<Box<LinkedNode<T>>>) -> LinkedList<T> {
+        match node {
+            None => {
+                LinkedList { 
+                    head: None,
+                    length: 0
+                }
+            }
+            _ => {
+                LinkedList { 
+                    head: node,
+                    length: 1
+                }
+            },
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let linked_node=LinkedNode::new(1, Option::None);
+    let linked_list=LinkedList::new(Option::from(Box::from(linked_node)));
+    let empty_list: LinkedList<u32>=LinkedList::new(None);
+    println!("Populated LinkedList instance: {:#?}", linked_list);
+    println!("Empty LinkedList instance: {:#?}", empty_list);
 }
