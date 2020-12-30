0271badac6f4ba9129d4f7410d05034e1f1f00c0
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-12-30 23:14:15 +0100

    Implement push item at beginning of list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 type NextNode<T>=Box<ListNode<T>>;
 
 #[derive(Debug)]
@@ -16,14 +18,12 @@ pub struct ListNode<T> {
 pub struct LinkedList<T> {
     pub length: u64,
     head: Link<T>,
-    tail: Link<T>,
 }
 
 impl<T> LinkedList<T> {
     pub fn new() -> Self {
         LinkedList { 
             head: Link::Empty,
-            tail: Link::Empty,
             length: 0
         }
     }
@@ -32,22 +32,25 @@ impl<T> LinkedList<T> {
             Link::Empty => {
                 LinkedList { 
                     head: Link::Empty,
-                    tail: Link::Empty,
                     length: 0
                 }
             }
             _ => {
                 LinkedList { 
                     head: node,
-                    tail: Link::Empty,
                     length: 1
                 }
             },
         }
     }
-    pub fn push(&mut self, _value: T) {
-        unimplemented!();
+    pub fn push(&mut self, value: T) {
+        let new_node = Box::new( ListNode {
+            value,
+            next: mem::replace(&mut self.head, Link::Empty)
+        });
+        self.head = Link::More(new_node);
     }
+
     pub fn pop(&mut self) -> Option<T> {
         unimplemented!();
     }
@@ -59,9 +62,11 @@ mod tests {
 }
 
 fn main() {
-    let link=Link::More(Box::new(ListNode{ value: 1, next: Link::Empty}));
+    let link=Link::More(Box::new(ListNode{ value: "1", next: Link::Empty}));
     let linked_list=LinkedList::from(link);
-    let empty_list: LinkedList<u32>=LinkedList::from(Link::Empty);
+    let mut empty_list: LinkedList<u32>=LinkedList::from(Link::Empty);
     println!("Populated LinkedList instance: {:#?}", linked_list);
     println!("Empty LinkedList instance: {:#?}", empty_list);
+    empty_list.push(32);
+    println!("Pushed into LinkedList instance: {:#?}", empty_list);
 }
