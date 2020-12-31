441ae7696fb9d0ca3e280a2eaf11bf394c44e04b
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-12-31 22:42:52 +0100

    Cargo format and public from method

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use std::mem;
 
-type NextNode<T>=Option<Box<ListNode<T>>>;
+type NextNode<T> = Option<Box<ListNode<T>>>;
 
 #[derive(Debug)]
 pub struct ListNode<T> {
     value: T,
-    next: NextNode<T>
+    next: NextNode<T>,
 }
 
 #[derive(Debug)]
@@ -16,31 +16,27 @@ pub struct LinkedList<T> {
 
 impl<T> LinkedList<T> {
     pub fn new() -> Self {
-        LinkedList { 
+        LinkedList {
             head: None,
-            length: 0
+            length: 0,
         }
     }
-    fn from(node: Option<ListNode<T>>) -> Self {
+    pub fn from(node: Option<ListNode<T>>) -> Self {
         match node {
-            None => {
-                LinkedList { 
-                    head: None,
-                    length: 0
-                }
-            }
-            Some(listnode) => {
-                LinkedList { 
-                    head: Some(Box::new(listnode)),
-                    length: 1
-                }
+            None => LinkedList {
+                head: None,
+                length: 0,
+            },
+            Some(listnode) => LinkedList {
+                head: Some(Box::new(listnode)),
+                length: 1,
             },
         }
     }
     pub fn push(&mut self, value: T) {
         let new_node = Box::new(ListNode {
             value,
-            next: mem::replace(&mut self.head, None)
+            next: mem::replace(&mut self.head, None),
         });
         self.head = Some(new_node);
     }
@@ -61,14 +57,17 @@ mod tests {
     use super::*;
     #[test]
     fn basics() {
-        let node=ListNode{ value: "1", next: None};
+        let node = ListNode {
+            value: "1",
+            next: None,
+        };
 
-        let mut empty_list: LinkedList<&str>=LinkedList::from(None);
+        let mut empty_list: LinkedList<&str> = LinkedList::from(None);
 
         // Test empty list pop behaviour
         assert_eq!(empty_list.pop(), None);
         // Test adding pre-existing node elements using from
-        let mut linked_list=LinkedList::from(Some(node));
+        let mut linked_list = LinkedList::from(Some(node));
         let mut mutable_list = empty_list;
 
         mutable_list.push("1");
@@ -78,23 +77,23 @@ mod tests {
 
         // Create list of other types
         let mut list = LinkedList::new();
-         // Populate list
-         list.push(1);
-         list.push(2);
- 
-         // Check removal
-         assert_eq!(list.pop(), Some(2));
- 
-         // Push more items to check nothing has corrupted
-         list.push(3);
-         list.push(4);
- 
-         // Check normal removal
-         assert_eq!(list.pop(), Some(4));
-         assert_eq!(list.pop(), Some(3));
- 
-         // Check exhaustion
-         assert_eq!(list.pop(), Some(1));
-         assert_eq!(list.pop(), None);
+        // Populate list
+        list.push(1);
+        list.push(2);
+
+        // Check removal
+        assert_eq!(list.pop(), Some(2));
+
+        // Push more items to check nothing has corrupted
+        list.push(3);
+        list.push(4);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(3));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
