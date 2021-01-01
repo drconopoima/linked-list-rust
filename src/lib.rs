75c72841168adac4e0676de6bd820c69a7808b00
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-01-01 16:46:41 +0100

    Calculate list length field on pop/push

--- src/lib.rs
+++ src/lib.rs
@@ -37,11 +37,13 @@ impl<T> LinkedList<T> {
             next: self.head.take(),
         });
         self.head = Some(new_node);
+        self.length += 1;
     }
 
     pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
+            self.length -= 1;
             node.value
         })
     }
@@ -74,15 +76,16 @@ mod tests {
         let mut list = LinkedList::new();
         // Populate list
         list.push(1);
+        assert_eq!(list.length, 1);
         list.push(2);
 
         // Check removal
         assert_eq!(list.pop(), Some(2));
-
+        assert_eq!(list.length, 1);
         // Push more items to check nothing has corrupted
         list.push(3);
         list.push(4);
-
+        assert_eq!(list.length, 3);
         // Check normal removal
         assert_eq!(list.pop(), Some(4));
         assert_eq!(list.pop(), Some(3));
@@ -90,5 +93,6 @@ mod tests {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+        assert_eq!(list.length, 0);
     }
 }
