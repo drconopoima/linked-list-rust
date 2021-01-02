7835c95a24e7974098a3ed448fc3b074accedcf7
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-01-02 20:53:07 +0100

    Add methods head, tail, append

--- src/third.rs
+++ src/third.rs
@@ -27,4 +27,28 @@ impl<T> LinkedList<T> {
             length: 0,
         }
     }
+    pub fn append(&self, value: T) -> LinkedList<T> {
+        LinkedList {
+            head: Some(Rc::new(ListNode {
+                value,
+                next: self.head.clone(),
+            })),
+            length: self.length + 1,
+        }
+    }
+    pub fn tail(&self) -> LinkedList<T> {
+        LinkedList {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+            length: {
+                if self.length > 1 {
+                    self.length - 1
+                } else {
+                    0
+                }
+            },
+        }
+    }
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.value)
+    }
 }
