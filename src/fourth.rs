1ad4b7ec076981e39b1d7529a1eed4acdc5d398d
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-01-03 00:29:35 +0100

    Implemente push_front in fourth::LinkedList

--- src/fourth.rs
+++ src/fourth.rs
@@ -13,3 +13,41 @@ pub struct LinkedList<T> {
     tail: Link<T>,
     length: usize,
 }
+
+impl<T> ListNode<T> {
+    pub fn new(value: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(ListNode {
+            value,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            head: None,
+            tail: None,
+            length: 0,
+        }
+    }
+    pub fn push_front(&mut self, value: T) {
+        let new_head = ListNode::new(value);
+        match self.head.take() {
+            None => {
+                // empty-list, need to set the tail
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+                self.length += 1;
+            }
+            Some(old_head) => {
+                // non-empty list, need to connect the old_head
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+                self.length += 1;
+            }
+        }
+    }
+}
